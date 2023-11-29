a06ae88cc36b6e0d8c5c88b2514bedaba32a1cc7
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-11-28 20:05:06 -0600

    fixed add incident

--- rest_server.mjs
+++ rest_server.mjs
@@ -57,7 +57,7 @@ function dbRun(query, params) {
                 reject(err);
             }
             else {
-                resolve();
+                resolve("successfully added case");
             }
         });
     });
@@ -163,8 +163,8 @@ app.get('/incidents', (req, res) => {
 
 // PUT request handler for new crime incident
 app.put('/new-incident', (req, res) => {
-    console.log(req.body); // uploaded data
-    let data = req.body;
+    console.log(req.query); // uploaded data
+    let data = req.query;
     
     let dateTime = data.date + data.time;
     console.log(dateTime);
@@ -173,17 +173,16 @@ app.put('/new-incident', (req, res) => {
     // if statement that sends status 500 if case number already exists
     // data fields: case_number, date, time, code, incident, police_grid, neighborhood_number, block
     // need to break up the data fields in the request to and use set to insert 
-    sqlQuery.set({case_number: data.case_number, date_time: dateTime, code: data.code, incident: data.incident, police_grid: data.police_grid, neighborhood_number: data.neighborhood_number, block: data.block}); 
-    
-    console.log(dbRun(sqlQuery.build(), data));
-    dbRun(sqlQuery.build(), data).then(data => {
+    sqlQuery.set({case_number: '?', date_time: '?', code: '?', incident: '?', police_grid: '?', neighborhood_number: '?', block: '?'});
+    let params = [data.case_number,dateTime,data.code,data.incident,data.police_grid,data.neighborhood_number,data.block];
+
+    dbRun(sqlQuery.build(), params).then(data => {
         res.status(200).type('json').send(data);
         console.log("sent");
     }).catch(err => {
         res.status(500).type('text').send(err);
     });
 
-    res.status(200).type('txt').send('OK'); // <-- you may need to change this
 });
 
 // DELETE request handler for new crime incident
@@ -200,7 +199,7 @@ app.delete('/remove-incident', (req, res) => {
             res.status(500).type('text').send('case not found');
         }else{
             dbRun(sqlDelete.build(), [req.query.case_number]).then(response => {
-                res.status(200).type('text').send(values);
+                res.status(200).type('json').send(values);
             }).catch(err => {
                 res.status(500).type('text').send(err);
             });
